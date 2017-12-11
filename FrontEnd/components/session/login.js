45cff57cbf9d6ec3cdd836cbc20933b84c828e70
Patrick Conde <patrick.conde.costas@gmail.com>
2017-12-11 11:04:01 -0800

    adding errors

--- FrontEnd/components/session/login.js
+++ FrontEnd/components/session/login.js
@@ -1,6 +1,7 @@
 // jshint esversion: 6
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, ScrollView, TextInput, TouchableOpacity} from 'react-native';
+import { View, StyleSheet, Text, ScrollView, TextInput, TouchableOpacity,
+        AppState } from 'react-native';
 // import { login } from '../../actions/session_actions';
 
 class LoginScreen extends React.Component {
@@ -15,7 +16,20 @@ class LoginScreen extends React.Component {
     this.props.login(user);
   }
 
+  componentWillMount() {
+    AppState.addEventListener('change', this.props.clearErrors)
+  }
+
+  renderErrors() {
+    if (this.props.errors.length === 0) {
+
+    } else {
+
+    }
+  }
+
   render () {
+    console.log(this.props);
     console.log(this.state);
     return (
       <View style={styles.container}>
@@ -39,6 +53,7 @@ class LoginScreen extends React.Component {
         <TouchableOpacity onPress={(e) => this.handleLogin(e)}>
           <Text>Log in</Text>
         </TouchableOpacity>
+
       </View>
     )
   }
@@ -47,24 +62,25 @@ class LoginScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 64
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20
   },
   inputField: {
     color: 'black',
-           flex: 1,
-           width: 60,
-           height: 60,
-           fontSize: 16,
-           borderTopColor: 'blue'
+    flex: 1,
+    width: 60,
+    height: 60,
+    fontSize: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: 'blue'
   },
   inputContainer: {
     borderBottomColor: '#9E7CE3',
-       borderBottomWidth: 1,
-       flex: 1,
-       flexDirection: 'row',
-       marginBottom: 10
+    borderBottomWidth: 1,
+    flex: 1,
+    flexDirection: 'row',
+    marginBottom: 10
   }
 });
 export default LoginScreen;
