3ff81011c89d443da8c2d06c136303f03bc7ec66
Patrick Conde <patrick.conde.costas@gmail.com>
2017-12-11 21:19:23 -0800

    Deleted backend folder, moved it to different repo

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://dream-capture.herokuapp.com';
+export const API_URL = 'https://blyndd8.herokuapp.com/';
 
 export const SESSION_URL = `${API_URL}/api/session`;
 export const USERS_URL = `${API_URL}/api/users`;
