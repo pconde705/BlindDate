b0c58b5cbbc9487778a8580280f87b188931dbc2
Patrick Conde <patrick.conde.costas@gmail.com>
2017-12-12 23:12:05 -0800

    Fixed Errors

--- frontend/android/app/src/main/java/com/frontend/MainApplication.java
+++ frontend/android/app/src/main/java/com/frontend/MainApplication.java
@@ -3,7 +3,6 @@ package com.frontend;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.brentvatne.react.ReactVideoPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
             new ImagePickerPackage(),
             new RNFetchBlobPackage(),
             new ReactVideoPackage()
