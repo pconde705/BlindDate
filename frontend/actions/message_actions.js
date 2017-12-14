5a0ed789d1ccb1919ead95ddd0b08bc17cdc0147
Andrew Cho <andrewopes789@gmail.com>
2017-12-14 10:14:02 -0800

    Update actions

---
+++ frontend/actions/message_actions.js
@@ -0,0 +1,26 @@
+import { USERS_URL } from '../api/api';
+import axios from 'axios';
+
+export const RECEIVE_MESSAGES = 'RECEIVE_MESSAGES';
+export const RECEIVE_MESSAGE = 'RECEIVE_MESSAGE';
+export const START_LOADING_MESSAGES = 'START_LOADING_MESSAGES';
+
+const receiveMessages = messages => ({
+  type: RECEIVE_MESSAGES,
+  messages
+});
+
+const receiveMessage = message => ({
+  type: RECEIVE_MESSAGE,
+  message
+});
+
+export const fetchMessages = userId => dispatch => (
+  axios.get(`${USERS_URL}/${userId}/messages`, {})
+  .then(response => dispatch(receiveMessages(response.data)))
+);
+
+export const createMessage = (inputMessage, userId) => dispatch => (
+  axios.post(`${USERS_URL}/${userId}/messages`, { inputMessage })
+  .then(response => dispatch(receiveMessage(response.data)))
+);
