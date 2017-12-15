b56a526eb1a4a2675f222dc654ca25968e86fb13
Patrick Conde <patrick.conde.costas@gmail.com>
2017-12-14 22:56:15 -0800

    Adding chat functionality

--- frontend/components/chat_room/chat_room.js
+++ frontend/components/chat_room/chat_room.js
@@ -8,22 +8,45 @@ class ChatRoom extends React.Component {
     this.state = { body: "", messages: [] }
   }
 
+  componentDidMount() {
+    let arr = Object.values(this.props.messages);
+    let empty = [];
+    arr.map(message => {
+      if (this.props.navigation.state.params.user.match_first_name === message.recipient_first_name) {
+        empty.push({_id: message.id, text: message.body,
+          user: { _id: this.props.navigation.state.params.user.match_id,
+            name: message.recipient_first_name} });
+      } else if (this.props.navigation.state.params.user.match_first_name === message.sender_first_name) {
+        empty.push({_id: message.id, text: message.body,
+          user: { _id: this.props.navigation.state.params.user.match_id,
+            name: message.sender_first_name} });
+      }
+    });
+    this.setState({messages: empty});
+  }
+
+
   onSend(messages = []) {
+    const stateTemp = {body: messages[0].text}
+    const newMessage = Object.assign({}, stateTemp)
+    this.props.createMessage(newMessage);
     this.setState((previousState) => ({
       messages: GiftedChat.append(previousState.messages, messages),
     }));
   }
 
-  // componentWillMount() {
-  //   this.props.fetchMessages();
-  // }
-  //
+  componentWillMount() {
+    this.props.fetchMessages(this.props.currentUser.id);
+  }
+
   componentWillReceiveProps(nextProps) {
     console.log(nextProps);
     // nextProps.fetchMessages();
   }
 
   render () {
+    console.log(this.state.messages);
+    console.log(this.props.messages);
     console.log(this.props.navigation.state.params.user);
     // text={(body) => this.setState({body})}
     // onInputTextChanged={(body) => this.setState({body})}
@@ -32,7 +55,7 @@ class ChatRoom extends React.Component {
         messages={this.state.messages}
         onSend={(messages) => this.onSend(messages)}
         user={{
-          _id: 1,
+          _id: this.props.currentUser.id, name: this.props.currentUser.name
         }}
       />
     )
